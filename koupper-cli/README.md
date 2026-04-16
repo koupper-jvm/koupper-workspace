0fe80a85142435e5f15c98d287dc5795bbdb6ddf
Jacob G. Acosta <jacob.gacosta@gmail.com>
2026-04-15 18:46:12 -0600

    chore(workspace): fully untrack nested koupper repos from root

--- koupper-cli/README.md
+++
@@ -1,2 +0,0 @@
-# Koupper cli
-Is an assistant for your local host environment bonded to Koupper-Architecture.
--- koupper-cli/src/main/kotlin/com/koupper/cli/buildtools/MavenOption.kt
+++
@@ -1,9 +0,0 @@
-package com.koupper.cli.buildtools
-
-import com.koupper.cli.Wizard
-
-class MavenOption : Wizard {
-    override fun init(args: Map<String, String>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-}
